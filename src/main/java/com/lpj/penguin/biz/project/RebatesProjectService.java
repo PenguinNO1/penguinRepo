9def4c1427a2685c64a88577cda23735baeac6f7
李培杰 <pjli5@mail.yst.com.cn>
2016-01-14 09:31:12 +0800

    修改路径名称

---
+++ src/main/java/com/lpj/penguin/biz/project/RebatesProjectService.java
@@ -0,0 +1,11 @@
+package com.lpj.penguin.biz.project;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 返利项目 Service
+ * Created by free9 on 15/12/5.
+ */
+@Component
+public class RebatesProjectService {
+}
---
+++ src/main/java/com/lpj/penguin/common/exception/ShowInfoException.java
@@ -0,0 +1,76 @@
+package com.lpj.penguin.common.exception;
+
+import java.io.Serializable;
+
+/**
+ * 类名:		NFSQException
+ * 描述:		用于异常的封装
+ * @author 	guoquanyao
+ *
+ */
+public class ShowInfoException extends RuntimeException implements Serializable {
+    private static final long serialVersionUID = -6844582544527740853L;
+
+    private String            errorCode;
+
+    private String            message;
+
+    private Object[]          args;
+
+    /**
+     * NFSQException构造函数
+     *
+     * @param errorCode
+     */
+    public ShowInfoException(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ShowInfoException(String errorCode, Object... args) {
+        this.errorCode = errorCode;
+        this.args = args;
+    }
+
+    /**
+     * @return 返回变量errorCode的值
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * @param errorCode 设置errorCode的值
+     */
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * @return 返回变量args的值
+     */
+    public Object[] getArgs() {
+        return args;
+    }
+
+    /**
+     * @param args 设置args的值
+     */
+    public void setArgs(Object... args) {
+        this.args = args;
+    }
+
+    /**
+     * @return 返回变量message的值
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message 设置message的值
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
